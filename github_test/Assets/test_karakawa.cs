ec450a4dc00dbf7cdeb7446771c66fc8896e968e
Karamjuice <Karapie0421@gmail.com>
2025-07-19 18:13:52 +0900

    testc#file create

---
+++ github_test/Assets/test_karakawa.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_karakawa : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()#Å‰‚É—ˆ‚é
+    {
+        Debug.Log("a");
+    }
+
+    // Update is called once per frame
+    void Update()#ƒ‹[ƒvŠÖ”“I‚È“z
+    {
+        
+    }
+}
